62aa5bbdf621144c4fb936d3b3d02bb566f08052
astghik <astghik.hakopian@gmail.com>
2019-02-26 02:41:02 +0400

    setup project

---
+++ qqq/src/qqq/SimpleEx.java
@@ -0,0 +1,28 @@
+package qqq;
+
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+
+public class SimpleEx extends JFrame {
+
+    public SimpleEx() {
+
+        initUI();
+    }
+
+    private void initUI() {
+        
+        setTitle("Simple example");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            SimpleEx ex = new SimpleEx();
+            ex.setVisible(true);
+        });
+    }
+}
